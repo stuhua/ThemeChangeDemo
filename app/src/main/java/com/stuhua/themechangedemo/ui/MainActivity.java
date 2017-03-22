91ddedba454a69b7de3ebcaac9125e2e09359204
liulianhua <liulianhua@navchina.com>
2017-03-22 12:05:11 +0800

    change something

--- app/src/main/java/com/stuhua/themechangedemo/ui/MainActivity.java
+++ app/src/main/java/com/stuhua/themechangedemo/ui/MainActivity.java
@@ -3,20 +3,35 @@ package com.stuhua.themechangedemo.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 import com.orhanobut.logger.Logger;
 import com.stuhua.themechangedemo.R;
 
 public class MainActivity extends AppCompatActivity {
+    public Button btn1, btn2, btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme_Night);
         setContentView(R.layout.activity_main);
+    }
+
+    public void initView() {
+        btn1 = getViewByID(R.id.btn1);
+        btn2 = getViewByID(R.id.btn2);
+        btn3 = getViewByID(R.id.btn3);
+
+    }
+
+    public <T extends View> T getViewByID(int id) {
+        return (T) findViewById(id);
     }
 
     public void changeTheme1(View view) {
         Logger.d("view=" + view.getId());
+        MainActivity.this.recreate();
     }
 
     public void changeTheme2(View view) {
