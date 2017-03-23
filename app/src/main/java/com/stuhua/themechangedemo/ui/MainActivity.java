0032b9f175186b6fc9c92829f79990e686387f0b
liulianhua <liulianhua@navchina.com>
2017-03-23 09:35:11 +0800

    添加第二种方法

--- app/src/main/java/com/stuhua/themechangedemo/ui/MainActivity.java
+++ app/src/main/java/com/stuhua/themechangedemo/ui/MainActivity.java
@@ -1,5 +1,7 @@
 package com.stuhua.themechangedemo.ui;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,11 +11,16 @@ import com.orhanobut.logger.Logger;
 import com.stuhua.themechangedemo.R;
 import com.stuhua.themechangedemo.utils.SPUtils;
 
+import java.lang.reflect.Method;
+
+import dalvik.system.DexClassLoader;
+
 public class MainActivity extends AppCompatActivity {
     public Button btn1, btn2, btn3;
     public static final String KEY_MODE1 = "mode1";
     public static final String KEY_MODE2 = "mode2";
     public static final String KEY_MODE3 = "mode3";
+    private DexClassLoader mDexClassLoader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
             setTheme(R.style.MyThemeNight);
         }
         setContentView(R.layout.activity_main);
+        initView();
     }
 
     public void initView() {
@@ -38,6 +46,21 @@ public class MainActivity extends AppCompatActivity {
         return (T) findViewById(id);
     }
 
+    /**
+     * 通过DexClassLoader加载dex中的xx方法
+     */
+    private void setContent() {
+        try {
+            Class clazz = mDexClassLoader.loadClass("com.stuhua.resource.UIUtil");
+            Method method = clazz.getMethod("getTextString", Context.class);
+            String str = (String) method.invoke(null, this);
+            btn2.setText(str.toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public void changeTheme1(View view) {
         Logger.d("view=" + view.getId());
         boolean mode = (boolean) SPUtils.get(this, KEY_MODE1, true);
@@ -46,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void changeTheme2(View view) {
-
+        startActivity(new Intent());
     }
 
     public void changeTheme3(View view) {
